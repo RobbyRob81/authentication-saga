4d9b5945cc9c1349b137ed71af233b4674c84a3a
Rob.Rosario <robbysrosario@gmail.com>
2019-01-03 07:30:44 -0800

    update: added http.service

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,3 +1,10 @@
+/*
+  Purpose:
+  1. combine into one main saga (rootSaga)
+  2. Perform non-blocking operation on the functions passed using fork
+
+*/
+
 import {
   watchAuth
 } from './auth.js'
