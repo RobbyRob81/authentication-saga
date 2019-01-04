4fa7eaf1c17d1604d255841cb06200e0f717fc26
Rob.Rosario <robbysrosario@gmail.com>
2019-01-04 07:07:02 -0800

    update: updated react import to use * as React

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 import { Router, Route } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
